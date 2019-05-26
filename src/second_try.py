c3ef13e16d3f5af7b11f2cf257d6d6ac92c47202
David <3kirchha@informatik.uni-hamburg.de>
2019-05-26 20:23:20 +0200

    model fix, 300 epochs traning, test images predictions

--- src/second_try.py
+++ src/second_try.py
@@ -1,13 +1,11 @@
 import numpy as np
+from tensorflow import keras
 import tensorflow as tf
-import tensorflow.keras as keras
 import os
 from matplotlib import pyplot as plt
 import cv2
-import math
 
-batch_size = 4
-
+batch_size = 16
 
 def load_images_from_folder(folder):
     images = []
@@ -15,27 +13,39 @@ def load_images_from_folder(folder):
         img = cv2.imread(os.path.join(folder, filename))
         if img is not None:
             images.append(img)
-    return np.asarray(images)
+    return np.asarray(images).astype('float32') / 255.0
 
 
-train_images = load_images_from_folder('cv2_data/train/images')[:120]
-train_images_fixation = load_images_from_folder('cv2_data/train/fixations')[:120]
+train_images = load_images_from_folder('cv2_data/train/images')
+train_images_fixation = load_images_from_folder('cv2_data/train/fixations')
+val_images = load_images_from_folder('cv2_data/val/images')
+val_images_fixation = load_images_from_folder('cv2_data/val/fixations')
+test_images = load_images_from_folder('cv2_data/test/images')
 
-val_images = load_images_from_folder('cv2_data/val/images')[:60]
-val_images_fixation = load_images_from_folder('cv2_data/val/fixations')[:60]
-
-test_images = load_images_from_folder('cv2_data/test/images')[:10]
-
-print(train_images.shape)
-print(train_images_fixation[0].shape)
-print(test_images.shape)
-print(train_images[50:60].shape)
+# train_images = train_images.astype('float32') 
+# train_images_fixation = train_images_fixation.astype('float32') 
+# val_images = val_images.astype('float32') 
+# val_images_fixation = val_images_fixation.astype('float32')
+# test_images = test_images.astype('float32')
+# 
+# train_images /= 255
+# train_images_fixation /= 255
+# val_images /= 255
+# val_images_fixation /= 255
+# test_images /= 255
 
 input_shape = train_images[0].shape
-print(input_shape)
+
+checkpoint_path_old = "training_300_epochs/cp.ckpt"
+checkpoint_path = "training_300_epochs/cp.ckpt"
+checkpoint_dir = os.path.dirname(checkpoint_path)
+
+# Create checkpoint callback
+cp_callback = tf.keras.callbacks.ModelCheckpoint(checkpoint_path, 
+                                                 save_weights_only=True,
+                                                 verbose=0)
 
 model = keras.models.Sequential()
-
 # first layer conv_1 180 x 320 #-> 192 x 320
 model.add(keras.layers.ZeroPadding2D(padding=(6, 0)))
 model.add(keras.layers.Conv2D(64, kernel_size=(3, 3), padding='same', activation='relu', data_format='channels_last', input_shape=input_shape))
@@ -51,13 +61,13 @@ model.add(keras.layers.MaxPool2D(pool_size=(2, 2)))
 model.add(keras.layers.Conv2D(256, kernel_size=(3, 3), padding='same', activation='relu'))
 model.add(keras.layers.Conv2D(256, kernel_size=(3, 3), padding='same', activation='relu'))
 model.add(keras.layers.Conv2D(256, kernel_size=(3, 3), padding='same', activation='relu'))
-model.add(keras.layers.MaxPool2D(pool_size=(2, 2), padding='valid'))
+model.add(keras.layers.MaxPool2D(pool_size=(2, 2)))
 
 # forth layer conv_4 24 x 40
 model.add(keras.layers.Conv2D(512, kernel_size=(3, 3), padding='same', activation='relu'))
 model.add(keras.layers.Conv2D(512, kernel_size=(3, 3), padding='same', activation='relu'))
 model.add(keras.layers.Conv2D(512, kernel_size=(3, 3), padding='same', activation='relu'))
-model.add(keras.layers.MaxPool2D(pool_size=(2, 2), padding='valid'))
+model.add(keras.layers.MaxPool2D(pool_size=(2, 2)))
 
 # fifth layer conv_5 12 x 20
 model.add(keras.layers.Conv2D(512, kernel_size=(3, 3), padding='same', activation='relu'))
@@ -96,36 +106,29 @@ model.add(keras.layers.Cropping2D(cropping=(6, 0)))
 model.add(keras.layers.Conv2D(1, kernel_size=(1, 1), activation='sigmoid'))
 
 model.compile(loss=keras.losses.binary_crossentropy,
-              optimizer=keras.optimizers.Adam(), metrics=['accuracy'])
+              optimizer=keras.optimizers.SGD(lr=0.001))
 
-model.fit(train_images, train_images_fixation, batch_size=batch_size, epochs=1,
-          verbose=1, validation_data=(val_images, val_images_fixation))
-# serialize model to JSON
-model_json = model.to_json()
-with open("model.json", "w") as json_file:
-    json_file.write(model_json)
-# serialize weights to HDF5
-model.save_weights("model.h5")
+#model.load_weights(checkpoint_path_old)
 
-#json_file = open('model.json', 'r')
-#loaded_model_json = json_file.read()
-#json_file.close()
-#loaded_model = keras.models.model_from_json(loaded_model_json)
-# load weights into new model
-#loaded_model.load_weights("model.h5")
-#print("Loaded model from disk")
+#model.fit(train_images, train_images_fixation, batch_size=batch_size, epochs=50,
+#        verbose=1, validation_data=(val_images, val_images_fixation),
+#        callbacks = [cp_callback])
 
+model.load_weights(checkpoint_path)
 predictions = model.predict(test_images, batch_size=batch_size, verbose=1)
 
-print(predictions.shape)
 #print(model.summary())
 
-plt.imshow(train_images[50])
-plt.show()
+for idx, val in enumerate(test_images):
+    #plt.imshow(predictions[idx, :, :, 0], cmap='gray')
+    f, axarr = plt.subplots(2)
+    axarr[0].imshow(test_images[idx])
+    axarr[1].imshow(predictions[idx, :, :, 0], cmap='gray')
 
-plt.imshow(predictions[0, :, :, 0], cmap='gray')
-plt.show()
+    #axarr[0].imshow(train_images[idx])
+    #axarr[1].imshow(train_images_fixation[idx])
+    #axarr[2].imshow(predictions[idx, :, :, 0], cmap='gray')
 
-plt.imshow(train_images_fixation[50], cmap='gray')
-plt.show()
-
+    plt.savefig('test_300epochs_predictions_{}.png'.format(idx))
+    plt.close()
+    #plt.show()
