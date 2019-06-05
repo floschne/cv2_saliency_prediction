ba0d2be1e02a24070cf48092581976d5bad57230
floschne <florian.schneider.1992@gmx.de>
2019-06-05 15:54:17 +0200

    removed date from folder..

--- src/second_try.py
+++ src/second_try.py
@@ -49,7 +49,7 @@ def main(operation, batch_size, train_size, epochs, data_dir, model_dir, log_dir
         print("train_images[0].shape: " + str(train_images[0].shape))
 
         # Create checkpoint callback
-        cp_callback = tf.keras.callbacks.ModelCheckpoint(os.path.join(os.path.abspath(checkpoint_path), str(datetime.datetime.now().strftime("%Y%m%d-%H%M%S"))),
+        cp_callback = tf.keras.callbacks.ModelCheckpoint(checkpoint_path,
                                                          save_weights_only=False,
                                                          period=50,
                                                          verbose=0)
